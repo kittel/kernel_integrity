9375e2fd6d1342a661ed59f30b528e35931d33ca
Thomas Kittel <kittel@sec.in.tum.de>
2015-03-12 16:18:14 +0100

    Added constants for colored output

--- src/helpers.h
+++ src/helpers.h
@@ -8,6 +8,17 @@
 #include <sstream>
 #include <vector>
 
+#define	COLOR_NORM      "\033[1;m"  
+#define	COLOR_GRAY      "\033[1;30m"
+#define	COLOR_RED       "\033[1;31m"
+#define	COLOR_GREEN     "\033[1;32m"
+#define	COLOR_YELLOW    "\033[1;33m"
+#define	COLOR_BLUE      "\033[1;34m"
+#define	COLOR_MARGENTA  "\033[1;35m"
+#define	COLOR_CYAN      "\033[1;36m"
+#define	COLOR_WHITE     "\033[1;37m"
+#define	COLOR_CRIMSON   "\033[1;38m"
+
 inline std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
     std::stringstream ss(s);
     std::string item;
